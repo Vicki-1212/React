1324d3e43c14a0e42261d1a3e1c796930b0120b8
Vignesh <vicki.k1212@gmail.com>
2025-04-02 18:28:32 +0530

    fetch the data in API

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,30 +1,61 @@
 import RestaurantContainer from "./RestaurantContainer";
 import { data } from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Body = () => {
 
-    const [listOfRes, setListOfRes] = useState(data);
+    const [listOfRes, setListOfRes] = useState([]);
+
+    const [searchValue, setSearchValue] = useState("");
+
+    const handleSearch = () => {
+        const filteredList = listOfRes.filter((res) => res.name.toLowerCase().includes(searchValue.toLowerCase()));
+        if (filteredList.length > 0) {
+            setListOfRes(filteredList);
+        } else {
+            setListOfRes(data);
+        }
+    }
+
+    const fetchData = async () => {
+        const data = await fetch('https://proxy.corsfix.com/?https://www.swiggy.com/mapi/restaurants/list/v5?offset=0&is-seo-homepage-enabled=true&lat=12.9939595&lng=80.1706653&carousel=true&third_party_vendor=1');
+        
+        const jsonData = await data.json();
+        const restaurants = jsonData.data.cards[4].card.card.gridElements.infoWithStyle.restaurants;
+        if (restaurants) {
+            setListOfRes(restaurants);
+        }
+        else {
+            setListOfRes([])
+        }
+        
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
 
     return (
         <div className="body-container">
             <div className="search-container">
-                <input type="search" className="input-search" />
-                <button className="search-button">Search</button>
+                <input type="search" className="input-search" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
+                <button className="search-button" onClick={handleSearch}>Search</button>
                 <button className="filter-button" onClick={() => {
                     const filteredList = listOfRes.filter((res) => res.avgRating > 4);
                     setListOfRes(filteredList);
                 }}>Top Rated Restaurants</button>
             </div>
             <div className="res-container">
-                {listOfRes.map((item) => (
+                {listOfRes?.map((item) => (
                     <RestaurantContainer
-                        key={item.id}
-                        resName={item.name}
-                        cuisine={item.cuisines.join(", ")}
-                        image={item.image}
-                        rating={item.avgRating}
-                        deliveryTime={item.deliveryTime}
+                        key={item?.info?.id}
+                        resName={item?.info?.name}
+                        cuisine={item?.info?.cuisines?.join(",")}
+                        image={item?.info?.cloudinaryImageId}
+                        rating={item?.info?.avgRating}
+                        deliveryTime={item?.info?.sla?.deliveryTime}
+                        costForTwo={item?.info?.costForTwo}
                     />
                 ))}
             </div>
--- src/Components/RestaurantContainer.js
+++ src/Components/RestaurantContainer.js
@@ -1,16 +1,18 @@
+import { IMG_URL } from "../utils/constant";
+
 const RestaurantContainer = (props) => {
     return (
       <div className="res-card">
         <img
           className="res-image"
-          src={props.image}
+          src={IMG_URL + props.image}
           alt="res-image"
         />
         <h2>{props.resName}</h2>
         <h4>{props.cuisine}</h4>
         <h4>{props.rating} stars</h4>
         <h4>{props.deliveryTime} minutes</h4>
-        <h4>Rs.500 for two</h4>
+        <h4>{props.costForTwo}</h4>
       </div>
     );
 };
